3272a5c4927fb4b11554bf87b5bebc8ea0db67a1
ANUSHALAV <bhattanushalav@gmail.com>
2026-06-13 17:27:58 +0530

    add navbar,hero,about,education,experiance,contact,footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
+import { Hero } from './layout/hero/hero';
+import { About } from './layout/about/about';
+import { Education } from './layout/education/education';
+import { Experiance } from './layout/experiance/experiance';
+import { Project } from './layout/project/project';
+import { Contact } from './layout/contact/contact';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Hero, About, Education, Experiance, Project, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
