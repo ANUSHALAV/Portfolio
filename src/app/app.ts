ed8bf83d07a363afd336e5ce59f55158f509ed9b
ANUSHALAV <bhattanushalav@gmail.com>
2026-06-20 22:14:56 +0530

    add basic code in navbar, hero , about,contact,footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Navbar } from './layout/navbar/navbar';
-import { Hero } from './layout/hero/hero';
-import { About } from './layout/about/about';
-import { Education } from './layout/education/education';
-import { Experiance } from './layout/experiance/experiance';
-import { Project } from './layout/project/project';
-import { Contact } from './layout/contact/contact';
-import { Footer } from './layout/footer/footer';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { Navbar } from "./layout/navbar/navbar";
+import { Hero } from "./layout/hero/hero";
+import { About } from "./layout/about/about";
+import { Education } from "./layout/education/education";
+import { Contact } from "./layout/contact/contact";
+import { Footer } from "./layout/footer/footer";
+import { Project } from "./layout/project/project";
+import { Experiance } from "./layout/experiance/experiance";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero, About, Education, Experiance, Project, Contact, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [RouterOutlet,Navbar,Hero,About,Education,Contact,Footer,Project,Experiance]
 })
 export class App {
-  protected readonly title = signal('Portfolio');
 }
