cd73c83740bf3f1722a3c814bd2f07c091e1b712
ANUSHALAV <bhattanushalav@gmail.com>
2026-06-21 18:12:21 +0530

    add education section and chagnes some content

--- src/app/layout/education/education.ts
+++ src/app/layout/education/education.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface EducationSection {
+  degree: string;
+  institution: string;
+  duration: string;
+  description?: string;
+  highlights?: string[];
+}
+
 @Component({
   selector: 'app-education',
   imports: [],
@@ -7,5 +15,20 @@ import { Component } from '@angular/core';
   styleUrl: './education.scss',
 })
 export class Education {
-
+  educationList: EducationSection[] = [
+    {
+      degree: 'Master of Computer Application',
+      institution: 'Chadigrah Univercity',
+      duration: '2024 — 2026',
+      description: 'Advanced study focusing on full-stack application development architectures, software engineering practices, and cloud technologies.',
+      highlights: ['Full-Stack Web Dev', 'Software Quality Assurance', 'Cloud Computing Architecture']
+    },
+    {
+      degree: 'Bachelor of Science in Information Technology',
+      institution: 'Swami Purnanand Degree College',
+      duration: '2020 — 2023',
+      description: 'Built a foundational core in database management systems, procedural & object-oriented programming, and web utility development.',
+      highlights: ['Core Java / JS', 'Database Management (DBMS)', 'Data Flow & Logic Design']
+    }
+  ];
 }
